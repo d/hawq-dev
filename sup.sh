cae47f3c2b0749e5555bf750681f47ab2cc76ab6
Jesse Zhang <sbjesse@gmail.com>
2015-02-11 14:52:28 -0800

    First pass at making things work
    Spoiler: it does not work

---
+++ sup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -u -e
+set -x
+
+readonly APT_PROXY=http://192.168.177.128:3142
+
+install_packages() {
+	sed -i s/enabled=1/enabled=0/ /etc/yum/pluginconf.d/fastestmirror.conf
+	local readonly PACKAGES=(
+		flex
+		bison
+		make
+		java-1.7.0-openjdk-devel
+		perl
+
+		man
+		vim-enhanced
+		wget
+		curl
+		pam-devel
+	)
+
+	env http_proxy="${APT_PROXY}" yum install -y "${PACKAGES[@]}"
+}
+
+install_ivy() {
+	local readonly ANTLIB_PATH=~/.ant/lib
+	local readonly IVY_URL=http://supergsego.com/apache/ant/ivy/2.4.0/maven2/2.4.0/ivy-2.4.0.jar
+
+	mkdir -vp "${ANTLIB_PATH}"
+	pushd "${ANTLIB_PATH}"
+	wget --continue "${IVY_URL}"
+	popd
+}
+
+_make() {
+	pushd /orca
+
+	local readonly EXT_PATH=/orca/ext/rhel5_x86_64
+
+	local readonly GCC_ENV=(
+		"PATH=/opt/gcc-4.4.2/bin:/opt/gcc_infrastructure/bin:$EXT_PATH/python-2.6.2/bin:$PATH"
+		"LD_LIBRARY_PATH=/opt/gcc_infrastructure/lib:$EXT_PATH/python-2.6.2/lib:$EXT_PATH/lib"
+	)
+	export JAVA_HOME=/usr/lib/jvm/java
+	make sync_tools http_proxy="${APT_PROXY}"
+
+	make devel "${GCC_ENV[@]}" HOME="$(pwd)"
+
+	popd
+}
+
+_main() {
+	install_packages
+
+	install_ivy
+
+	_make
+}
+
+_main
